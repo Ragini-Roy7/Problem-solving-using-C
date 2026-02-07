4f61259ae7840c793ab619c2fcc2f2dd156a7263
Ragini-Roy7 <royragini967@gmail.com>
2026-02-07 21:09:45 +0530

    leetcode_problem added

--- array/array_revision/best_time_to_buy_sell_stock.c
+++ array/array_revision/best_time_to_buy_sell_stock.c
@@ -7,7 +7,7 @@ int main() {
     int min_price = prices[0];
     int max_profit = 0;
 
-    for(int i = 1; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         if(prices[i] < min_price)
             min_price = prices[i];
         else if(prices[i] - min_price > max_profit)
